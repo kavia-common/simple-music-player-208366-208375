4c4001df8b242c857a328338b90d772a299c0968
Kavia AI <production@kavia.ai>
2026-02-02 06:25:22 +0000

    Checkpoint 2 - cg3cab0a88

---
+++ music_player_frontend/.dart_tool/flutter_build/743d7fff14b457bfca0b357d2df6fa2c/dart_build.d
@@ -0,0 +1,1 @@
+ /home/kavia/workspace/code-generation/simple-music-player-208366-208375/music_player_frontend/.dart_tool/flutter_build/743d7fff14b457bfca0b357d2df6fa2c/dart_build_result.json: 
--- music_player_frontend/test/widget_test.dart
+++ music_player_frontend/test/widget_test.dart
@@ -1,18 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:music_player_frontend/main.dart';
 
 void main() {
-  testWidgets('App generation message displayed', (WidgetTester tester) async {
+  testWidgets('App renders main player screen', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    expect(find.text('music_player_frontend App is being generated...'), findsOneWidget);
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
-  });
-
-  testWidgets('App bar has correct title', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    expect(find.text('music_player_frontend'), findsOneWidget);
+    expect(find.text('Retro MP3 Player'), findsOneWidget);
+    expect(find.text('NOW PLAYING'), findsOneWidget);
+    expect(find.text('PLAY'), findsOneWidget);
   });
 }
